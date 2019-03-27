cf2b125c69634a58d19705b44c84774f2df13e9e
Alex BR <alex.bautista.ramos.90@gmail.com>
2019-03-26 23:22:19 -0600

    Reviewed sets, tuples and lists.

---
+++ sets-tuples-lists/sets-tuples-lists.py
@@ -0,0 +1,9 @@
+grades = [77, 80, 90, 100, 105, 107]
+
+# Tuples are immutable objects and they do not change, they remain the same at all times.
+tuples_grades = (77, 80, 90, 95, 100, 105, 107, 108, 109) 
+
+# Sets are unique and unordered objects. Although we have two 100s, only one will be printed. 
+set_grades = {77, 80, 90, 100, 100}
+
+print(set_grades)
