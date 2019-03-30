84bd42e7b758874ae8b6887841f73aefe9f97309
alexbr9007 <alex.bautista.ramos.90@gmail.com>
2019-03-30 13:38:22 -0600

    Added the instructions of the class and static methods.

--- static_class_methods/classmethod_staticmethod.py
+++ static_class_methods/classmethod_staticmethod.py
@@ -1,3 +1,23 @@
+'''
+This coding exercise requires you to complete two method implementations.
+1. The franchise method which takes in a store as argument. It should return a new Store object,
+with a name equal to the argument + " - franchise".
+2. The store_details method, which also takes in a store as argument. It should return a string
+representing the argument.
+
+store = Store("Test")
+store2 = Store("Amazon")
+store2. add_item("Keyboard", 160)
+
+Store.franchise(store)  # returns a Store with name "Test - franchise"
+Store.franchise(store2) # returns a Store with name "Amazon - franchise"
+
+Store.store_details(store)  # returns "Test" total stock price: 0"
+Store.store_details(store2) # returns "Amazon, total stock price: 160"
+
+'''
+
+
 class Store:
     def __init__(self, name):
         self.name = name
